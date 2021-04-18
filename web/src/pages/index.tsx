99a412341a749e3889cbe7e8e822b43f50c84c0b
Timeo1210 <timeo.boulhol@gmail.com>
2021-04-18 23:17:03 +0200

    feat(web): init web

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const IndexPage = () => (
+  <div title="Home | Next.js + TypeScript Example">
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </div>
+);
+
+export default IndexPage;
