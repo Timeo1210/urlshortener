5370b340ee62e7df37ee475374b67a0c72baa80e
Timeo1210 <timeo.boulhol@gmail.com>
2021-04-18 04:24:15 +0200

    feat(server): remove mongodb repository

--- server/src/infrastructure/repositories/init.ts
+++ server/src/infrastructure/repositories/init.ts
@@ -1,9 +1,9 @@
-import { mongodbRepository } from "./mongodb/config";
+// import { mongodbRepository } from "./mongodb/config";
 import { postgresqlRepository } from "./postgresql/config";
 
 export const initRepositories = {
   init: async (): Promise<void> => {
-    await mongodbRepository.run();
+    // await mongodbRepository.run();
     await postgresqlRepository.run();
   },
 };
