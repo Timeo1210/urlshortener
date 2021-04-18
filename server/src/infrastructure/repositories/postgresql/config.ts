fd849f52ff237179e0508387240999a4cd345af9
Timeo1210 <timeo.boulhol@gmail.com>
2021-04-18 04:23:46 +0200

    feat(server): add Hashlink migration support

--- server/src/infrastructure/repositories/postgresql/config.ts
+++ server/src/infrastructure/repositories/postgresql/config.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize";
 
+import HashlinkMigration from "./migrations/20210418015945-Hashlink";
+
 const isProd = process.env.NODE_ENV === "production" ? true : false;
 const uri =
   process.env.POSTGRESQL_URI ||
@@ -15,6 +17,7 @@ export const postgresqlRepository = {
     // db = new Sequelize(uri);
     await db.authenticate();
     await db.sync({ force: !isProd });
+    await HashlinkMigration.up(db.getQueryInterface());
     console.log("Connected to Postgresql");
   },
 };
