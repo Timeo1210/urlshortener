02514e697620d471e38f439cc84da09ecb007389
Timeo1210 <timeo.boulhol@gmail.com>
2021-04-18 04:49:41 +0200

    feat(server): prepare index for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,9 @@
 import "dotenv-safe/config";
 
-import { getLinkByHashService } from "@/core/services/getLinkbyHash.service";
-import { postLinkService } from "@/core/services/postLink.service";
-
 import { initControllers } from "@/interface-adapters/controllers/init";
 import { initRepositories } from "@/infrastructure/repositories/init";
 
 (async () => {
-  await initControllers.init();
   await initRepositories.init();
-  console.log(
-    "getLinkByHash:",
-    await getLinkByHashService.getLinkbyHash({ hash: "hhhhhhhh" })
-  );
-  console.log(
-    "postLink:",
-    await postLinkService.postLink({ link: "https://youtube.com" })
-  );
-
-  const generate = await postLinkService.postLink({
-    link: "https://www.codecademy.com/",
-  });
-  console.log(
-    "generate:",
-    await getLinkByHashService.getLinkbyHash({
-      hash: generate.redirect.slice(1, 8 + 1),
-    })
-  );
+  await initControllers.init();
 })();
