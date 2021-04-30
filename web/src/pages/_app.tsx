8478fd3fd9baa1cb1de10619ab8e9aeddb00a776
Timeo1210 <timeo.boulhol@gmail.com>
2021-05-01 00:27:06 +0200

    feat(web): add generate page

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "@/styles/globals.css";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
-    <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </>
 );
 
